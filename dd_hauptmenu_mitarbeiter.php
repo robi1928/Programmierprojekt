ddb1aa08941c9b54641e261b4399aee8738d97b8
Nic <niclasra.1999@googlemail.com>
2025-11-26 14:39:59 +0100

    Add files via upload

---
+++ dd_hauptmenu_mitarbeiter.php
@@ -0,0 +1,32 @@
+<!-- Standardseite, noch ohne Inhalt. Selber geschrieben -->
+<?php
+require_once 'bb_auth.php';
+include_once 'cc_benutzer.php';
+rolle_erforderlich(ROLLE_MITARBEITER);
+modus_aus_url_setzen();
+$benutzer = aktueller_benutzer();
+?>
+<!doctype html>
+<html lang="de"<?= html_modus_attribut() ?>>
+<head>
+  <meta charset="utf-8">
+  <title>Hauptmenü Mitarbeiter</title>
+  <link rel="stylesheet" href="aa_aussehen.css">
+</head>
+<body>
+  <h1>Hauptmenü Mitarbeiter</h1>
+  <p>Angemeldet: <?= htmlspecialchars($benutzer['name']) ?> (<?= htmlspecialchars($benutzer['rolle']) ?>)</p>
+  <nav>
+    <a href="bb_ausloggen.php">Abmelden</a>
+    <?= modus_navigation() ?>
+  </nav>
+  <?php $mitarbeiter = new CBenutzer($benutzer['id']); 
+  //Lade Daten muss noch implementiert werden - $mitarbeiter->LadeDaten();
+    echo '<p>Verbleibende Urlaubstage: '.$mitarbeiter->GetUrlaubstage().'</p>';
+  ?>
+  <div class="menu-links">
+    <a class="btn primary" href="dd_erfassung_mitarbeiter.php">Arbeitszeit einpflegen / bestätigen</a>
+    <a class="btn primary" href="dd_auswertung_mitarbeiter.php">Eigene Monatsübersicht</a>
+  </div>
+</body>
+</html>
